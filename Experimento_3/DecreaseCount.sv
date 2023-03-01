7b2cd2db3256cc01846159526a7334ba5f96a198
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2023-02-28 23:36:52 -0600

    Delete clk
    Se elimina el parametro del clk ya que este no es necesario en el funcionamiento del sistema

--- Experimento_3/DecreaseCount.sv
+++ Experimento_3/DecreaseCount.sv
@@ -1,17 +1,19 @@
 module DecreaseCount
-#(parameter N=6)
+#(parameter N=2)
 (
-  input wire clk,          // Clk para testeo
   input wire btn_decrement,// Boton para bajar el numero
   input wire btn_reset,    // Boton de reset
   output reg [2**N-1:0] count // El contador
 );
 
-  always @(posedge clk or negedge btn_decrement or negedge btn_reset) begin
+  always @(negedge btn_decrement or negedge btn_reset) begin //negedge transicion de 1 a 0 
     if (~btn_decrement) begin // Si el boton se presiona se baja en 1 count
       if (count > 0) begin // Si el valor es mayor que cero
         count <= count - 1;
       end
+	else begin
+		count <= 2**N - 1; // Volvemos al valor max
+	end
     end
 
     if (~btn_reset) begin // Si el reset se presiona
@@ -27,64 +29,61 @@ module DecreaseCount_tb;
   parameter N = 4;
   
   // Inputs
-  reg clk, btn_decrement, btn_reset;
+  reg btn_decrement, btn_reset;
   
   // Outputs
   wire [2**N-1:0] count;
   
   // Instanciamos el modulo
   DecreaseCount #(N) dut (
-    .clk(clk),
     .btn_decrement(btn_decrement),
     .btn_reset(btn_reset),
     .count(count)
   );
 
-  always #5 clk = ~clk; //clk con perido de 10 unidades de tiempo
-
   initial begin
-    clk = 0;
+
     btn_decrement = 0;
     btn_reset = 0;
     #10;
 
     // Test boton presionado
     btn_decrement = 1;
-    #5;
+    #15 $display("Count 1: %0d", count);
     btn_decrement = 0;
-    #5;
+    #15;
     btn_decrement = 1;
-    #5;
+    #15;
     btn_decrement = 0;
-    #5;
+    #15;
     btn_decrement = 1;
-    #5;
+    #15;
     btn_decrement = 0;
-    #5;
-	 #5 $display("Count: %0d", count);
+    #15;
+    #15 $display("Count 2: %0d", count);
 	 
     // Test reset
     btn_reset = 1;
-    #5;
+    #15;
     btn_reset = 0;
-    #5;
-	 #5 $display("Count: %0d", count);
+    #15;
+    #5 $display("Count 3: %0d", count);
 
     // Test boton presionado
     btn_decrement = 1;
-    #5;
+    #15;
     btn_decrement = 0;
-    #5;
+    #15;
     btn_decrement = 1;
-    #5;
+    #15;
     btn_decrement = 0;
-    #5;
+    #15;
     btn_decrement = 1;
-    #5;
+    #15;
     btn_decrement = 0;
-    #5;
-	 #5 $display("Count: %0d", count);
+    #15;
+    #15 $display("Count 4: %0d", count);
 
-    $finish; // Fin 
+    // Fin 
   end
 endmodule

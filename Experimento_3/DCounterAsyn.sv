09e20307c93c47a9785808ab9c9bb0199c1701eb
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2023-03-03 14:36:46 -0600

    Parte de simulacion
    De adjunta el codigo para testear el contador regresivo para N=2, N=4 y N=6

---
+++ Experimento_3/DCounterAsyn.sv
@@ -0,0 +1,27 @@
+module DCounterAsyn
+#(parameter N=6)
+(
+  input wire btn_increment,// Boton para aumentar el numero
+  input wire btn_decrement,// Boton para bajar el numero
+  input wire btn_reset,    // Boton de reset
+  output wire [2**N-1:0] count // El contador
+);
+	always @(negedge btn_decrement or negedge btn_reset or negedge btn_increment) begin //negedge transicion de 1 a 0 
+    if (~btn_decrement) begin // Si el boton se presiona se baja en 1 count
+      if (count > 0) begin // Si el valor es mayor que cero
+        count <= count - 1;
+      end
+		else begin
+			count <= 0; // Negativos se pone en cero
+		end
+    end
+	 if (~btn_increment) begin // Si el boton se presiona se sube en 1 count
+		 if (count < 2**N-1) begin // Si el valor es mayor que el valor max
+			 count <= count + 1;
+		 end
+	 end
+    if (~btn_reset) begin // Si el reset se presiona
+      count <= 2**N-1; // Volvemos al valor max
+    end
+  end
+endmodule

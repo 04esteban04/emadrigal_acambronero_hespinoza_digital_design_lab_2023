d342ea691ee98532102492e8e2063710ac63690c
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2023-03-06 19:08:04 -0600

    Cambios para la FPGA
    Se cambio un poco la logica para acoplarlo a la FPGA

--- Experimento_3/DCounterAsyn_tb.sv
+++ Experimento_3/DCounterAsyn_tb.sv
@@ -1,38 +1,33 @@
 module DCounterAsyn_tb;
-  // Parametros para el contador
-  localparam N2 = 2;
-  localparam N4 = 4;
-  localparam N6 = 6;
+
+	parameter N = 6;
+	reg [N-1:0] count;
+
+	logic btn_decrement;
+	logic btn_reset;
+	reg [6:0] segA;
+	reg [6:0] segB;
+	integer i;	
   
-  // Entradas al contador
-  logic btn_increment;
-  logic btn_decrement;
-  logic btn_reset;
+	DCounter #(.N(N)) counter (.btn_decrement(btn_decrement),
+										.btn_reset(btn_reset),
+										.count(count),
+										.segA(segA),
+										.segB(segB));
 
-  // Salidas del contador
-  reg [N2-1:0] count2;
-  reg [N4-1:0] count4;
-  reg [N6-1:0] count6;
+	// Inicializar entradas
+	initial begin
+		i = 0;
+		btn_decrement = 1;
+		btn_reset = 1;
+		#10 btn_reset = 0;
+		#10 btn_reset = 1;
+	 
+		for (i = 0; i < 9; i = i + 1) begin 
+			#10 btn_decrement = 0;
+			#10 btn_decrement = 1;
+		end
 
-  // Instanciar contadores para diferentes valores de N
-  DCounterAsyn #(N2) counter2(.btn_increment(btn_increment), .btn_decrement(btn_decrement), .btn_reset(btn_reset), .count(count2));
-  DCounterAsyn #(N4) counter4(.btn_increment(btn_increment), .btn_decrement(btn_decrement), .btn_reset(btn_reset), .count(count4));
-  DCounterAsyn #(N6) counter6(.btn_increment(btn_increment), .btn_decrement(btn_decrement), .btn_reset(btn_reset), .count(count6));
+	end
 
-  // Inicializar entradas
-  initial begin
-    #10 btn_increment = 1;
-    #10 btn_decrement = 1;
-    #10 btn_reset = 1;
-	 #10 btn_reset = 0;
-	 #10 btn_reset = 1;
-    #10 btn_decrement = 0;
-    #10 btn_decrement = 1;
-    #10 btn_increment = 0;
-    #10 btn_increment = 1;
-	 #10 btn_decrement = 0;
-    #10 btn_decrement = 1;
-	 #10 btn_decrement = 0;
-    #10 btn_decrement = 1;
-  end
 endmodule
